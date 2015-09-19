a640a283018bdefcb693189343fa99ec2aed9fbd
Sia <siakaramalegos@gmail.com>
2015-09-19 09:38:39 -0500

    Added description custom method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,9 @@
 class Invoice < ActiveRecord::Base
   belongs_to :customer
   has_many :payments
+
+  def description
+    # TODO:  make 2 decimal places
+    "#{number}: #{customer.name} $#{total_amount} USD due #{due_date}"
+  end
 end
