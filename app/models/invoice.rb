4c57763be8b863dadb0bfcd84095a4566bddf09d
Sia <siakaramalegos@gmail.com>
2015-09-19 21:12:16 -0500

    Added paid? method to invoice and adjusted views

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,4 +6,14 @@ class Invoice < ActiveRecord::Base
     # TODO:  make 2 decimal places
     "#{customer.name} invoice ##{number}"
   end
+
+  def paid?
+    all_payments = 0
+    payments.each {|pay| all_payments += pay.total_payment}
+    if all_payments >= (total_amount * 0.029) + 0.30
+      true
+    else
+      false
+    end
+  end
 end
