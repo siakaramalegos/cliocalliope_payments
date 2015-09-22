5bf1a7eeda641ec3d96733514140cad1ed2ce5ff
Sia <siakaramalegos@gmail.com>
2015-09-21 19:54:04 -0500

    Removed pop up display option for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret,
-  :scope => 'email', :display => 'popup', :info_fields => 'name,email'
+  :scope => 'email', :info_fields => 'name,email'
 end
